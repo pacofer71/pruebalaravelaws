690711ebf9d7c81a2e5c75cbf0480c2024464e75
Francisco fernández <pacofer71@gmail.com>
2022-06-15 20:03:47 +0000

    Dia 2

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowPosts extends Component
 {
+    use WithPagination;
+    public $campo="id", $orden="desc";
     public function render()
     {
-        return view('livewire.show-posts');
+
+        $posts=Post::where('user_id', auth()->user()->id)->orderBy($this->campo, $this->orden)->paginate(5);
+        return view('livewire.show-posts', compact('posts'));
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,3 +1,71 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="container mx-auto px-4 sm:px-8 max-w-7xl sm:px-6 lg:px-8">
+    <div class="py-8">
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table class="w-full leading-normal">
+                    <thead>
+                        <tr>
+                            <th scope="col"
+                                class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                Título
+                            </th>
+                            <th scope="col"
+                                class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                Contenido
+                            </th>
+                            <th scope="col"
+                                class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                Created at
+                            </th>
+                            <th scope="col"
+                                class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                                Publicado
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0">
+                                        <a href="#" class="block relative">
+                                            <img alt="profil" src="/images/person/8.jpg"
+                                                class="mx-auto object-cover rounded-full h-10 w-10 " />
+                                        </a>
+                                    </div>
+                                    <div class="ml-3">
+                                        <p class="text-gray-900 whitespace-no-wrap">
+                                            Jean marc
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">
+                                    Admin
+                                </p>
+                            </td>
+                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">
+                                    12/09/2020
+                                </p>
+                            </td>
+                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <span
+                                    class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                    <span aria-hidden="true"
+                                        class="absolute inset-0 bg-green-200 opacity-50 rounded-full">
+                                    </span>
+                                    <span class="relative">
+                                        active
+                                    </span>
+                                </span>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
